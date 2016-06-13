018928400cd903216ea0b93453814279104bceb1
Mark Griffiths <mark@thebespokepixel.com>
2016-06-13 05:11:07 +0100

    Snapshot @thebespokepixel/string 0.2.0-0-∆005

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,18 @@ gulp.task('bundle', cordial.macro({
 	source: 'src/index.es6'
 }).bundle())
 
+gulp.task('master', cordial.macro({
+	master: true,
+	source: 'src/lib/index.es6'
+}).bundle())
+
 // Tests
 gulp.task('ava', cordial.test().ava(['test/*.js']))
 gulp.task('xo', cordial.test().xo(['src/*.es6']))
 gulp.task('test', gulp.parallel('xo', 'ava'))
+
+gulp.task('post-flow-release-start', gulp.series('reset', 'master', 'version-release'))
+gulp.task('post-flow-release-finish', gulp.series('test', 'publish', 'push-force'))
 
 // Default
 gulp.task('default', gulp.series('bump', 'bundle'))
